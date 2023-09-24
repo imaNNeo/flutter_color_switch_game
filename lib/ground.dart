ffaf4f878560fd5ed112fc8a93b11c9e6b2de063
Iman Khoshabi <iman.neofight@gmail.com>
2023-09-24 18:23:57 +0200

    Add finger tap image instead of a red line in the ground position

--- lib/ground.dart
+++ lib/ground.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class Ground extends PositionComponent {
   static const String keyName = 'single_ground_key';
+
   Ground({required super.position})
       : super(
           size: Vector2(200, 2),
@@ -10,11 +11,20 @@ class Ground extends PositionComponent {
           key: ComponentKey.named(keyName),
         );
 
+  late Sprite fingerSprite;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    fingerSprite = await Sprite.load('finger_tap.png');
+  }
+
   @override
   void render(Canvas canvas) {
-    canvas.drawRect(
-      Rect.fromLTWH(0, 0, width, height),
-      Paint()..color = Colors.red,
+    fingerSprite.render(
+      canvas,
+      position: Vector2(56, 0),
+      size: Vector2(100, 100),
     );
   }
 }
